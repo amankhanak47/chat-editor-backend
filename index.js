9d88034affdc741e4c56fc915c9768dc089d632c
aman khan <mohdamankhan18@gmail.com>
2022-05-30 15:41:35 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const http=require("http")
+const express=require("express")
+const cors=require("cors")
+const socketIO=require("socket.io")
+
+
+const app = express()
+const port = 4500 || process.env.PORT
+
+app.use(cors());
+const users = [{}];
+
+
+app.get("/", (req, res) => {
+    res.send("working")
+})
+
+const server = http.createServer(app)
+
+
+const io = socketIO(server)
+
+io.on("connection", (socket) => {
+    console.log("new connection")
+
+    socket.on('joined', ({ user }) => {
+        users[socket.id] = user;
+        console.log(`${user} has joined`)
+        socket.broadcast.emit('userJoined',{user:"admin", message:`${users[socket.id]} has joined`})
+        socket.emit('welcome', {user:"admin", message:`Welcome to the chat ${users[socket.id]}`})
+    })
+
+
+    socket.on('message', ({message,id}) => {
+        io.emit('sendMessage',{user:users[id], message,id})
+       
+    })
+
+
+    socket.on('left', () => {
+
+      
+        socket.broadcast.emit('leave',{user:"admin",message:`${users[socket.id]} has left`})
+        
+
+    })
+
+    
+})
+
+
+server.listen(port, () => {
+    console.log(`server running on http://localhost:${port}`)
+})
